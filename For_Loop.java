c0ffc9e5b49acedf419bb27ed94127656d6e3bc7
eklavya <eklavyaprasad1@gmail.com>
2020-07-25 18:59:14 +0530

    Updated for loops with while loop info

--- For_Loop.java
+++ For_Loop.java
@@ -1,5 +1,8 @@
 class For_Loop{
     public static void main(String [] args){
         System.out.println("For Loop Tutorial");
+	
+	System.out.println("Done with for loop");
+	System.out.println("Wait what about while loops");
     }
 }
